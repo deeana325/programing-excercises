9c13186cb6b6bd60df5cfba2e949497fd08102ac
deeana325 <diana@192-168-0-117.rdsnet.ro>
2020-05-02 11:49:00 +0300

    compara vecini lista

--- Culegere IX-XI/1.139.py
+++ Culegere IX-XI/1.139.py
@@ -1,20 +1,25 @@
+import readchar
+
+
 def compar():
-    test = False
+    test = True
     contor = 0
-    numar = input('numar =')
-    numar_anterior = 0
-    while numar != 0:
-        if numar > numar_anterior:
-            if not test:
-                test = True
-            elif test:
+    numar = input('numar = ')
+    numar_anterior = '0'
+    while numar != '0':
+        if test and numar > numar_anterior:
                 contor += 1
                 test = False
-        else:
+        elif numar < numar_anterior:
             test = True 
         numar_anterior = numar
-        numar = input('numar =')
+        numar = input('numar = ')
     print('exista', contor, 'numere mai mari decat vecinii lor in sir')
+
+# ch = ''
+# while ch != '0':
+#     ch = readchar.readchar()
+#     print(ord(ch), ch)
 
 
 compar()
