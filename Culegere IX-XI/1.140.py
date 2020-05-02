3ee288b8a9de26f1e43c7833a3d7eb30fba8a2fe
deeana325 <diana@192-168-0-117.rdsnet.ro>
2020-05-02 11:49:39 +0300

    desc suma nr consecutive

--- Culegere IX-XI/1.140.py
+++ Culegere IX-XI/1.140.py
@@ -1,4 +1,18 @@
-def suma(n):
-    for i = 1 to n / 2 -1:
-        for j = 1 to :
-            sum(range(i,j))
+import sys
+
+
+def suma(n: int) -> list:
+    #write n as a sum of consecutive integers
+    rez = []
+    for i in range(1, n):
+        for j in range(i+1, n):
+            sum_i_to_j = (i + j)*(j - i + 1)//2 
+            if sum_i_to_j == n:
+                rez.append(list(range(i, j+1)))
+    return rez
+
+if suma(5) != [[2, 3]]:
+    print('Error')
+    sys.exit()
+assert suma(50) == [[8, 9, 10, 11, 12], [11, 12, 13, 14]], 'Wrong result'
+print(suma(20))
