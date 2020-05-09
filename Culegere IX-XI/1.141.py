96c4534d22761fadced9909370f81c3a0c9a4b0f
deeana325 <diana@192-168-0-117.rdsnet.ro>
2020-05-09 03:41:01 +0300

    inmultire de matrici cu numere consecutive de la 1 -> n, m

--- Culegere IX-XI/1.141.py
+++ Culegere IX-XI/1.141.py
@@ -1,9 +1,12 @@
+#   Pentru inmultirea A = {1, 2, 3, ..., m} si B = {1, 2, ..., n} 
+# sa se afiseze elementele multimii AxB = { (a,b) |a ∈ A, b ∈ B}.
+
 m = int(input("m = "))
 n = int(input("n = "))
-AxB = {}
+AxB = []
 
-for i in range(m):
-    for j in range(n):
+for i in range(1,m+1):
+    for j in range(1,n+1):
         AxB.append((i, j))
 
 print("AxB =", AxB)
