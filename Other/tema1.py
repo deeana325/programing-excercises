bd790eb008a963f61979cd9b46ad4fc5007f4b19
deeana325 <deeana.kris@gmail.com>
2020-08-24 09:14:17 +0300

    a chage

--- Other/tema1.py
+++ Other/tema1.py
@@ -1,13 +1,15 @@
-print('str= ')
-s= str(input())
-l= ['a','e','i','o','u']
-x=0
-for i in range(0,len(s)):
+print("str= ")
+s = str(input())
+l = ["a", "e", "i", "o", "u"]
+x = 0
+for i in range(0, len(s)):
     if s[i] in l:
-        x+=1
+        x += 1
 print(x)
 
 # for i in range(1,21):
 #     for j in range(1,21):
 #         for g in range(1,21):
 #             print(i,'*',j,'*',g,'= ',i*j*g)
+# *
+
