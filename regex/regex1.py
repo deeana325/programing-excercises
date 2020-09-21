3371433f208b567790a6535746b58ca5e9eb1816
deeana325 <deeana.kris@gmail.com>
2020-09-21 09:52:37 +0300

    finished

--- regex/regex1.py
+++ regex/regex1.py
@@ -2,14 +2,18 @@ import re
 
 path = "/Users/diana/Documents/School+/Python/regex/corpus.txt"
 
-# gaseste numere intregi, gaseste numere float, gaseste cuvinte care incep cu majuscula
-pattern = ["(?<intregi>(?<![\.0-9])[0-9]+(?![\.0-9]))", "(?<reale>[0-9]+\.[0-9]+)", "(?<cuv_majusc>(?<!\w)[A-Z][a-z]+(?!\w))"]
-
+# gaseste numere fractionare, gaseste numere intregi, gaseste cuvinte care incep cu majuscula
+p1 = re.compile(r"([0-9]+\.[0-9]+)")
+p2 = re.compile(r'((?<![\.0-9])[0-9]+(?![\.0-9]))')
+p3 = re.compile(r"((?<!\w)[A-Z][a-z]+(?!\w))")
+s = '1, 2, 3, 4 2.03, 1.74, 0.93 Ana are mere. Pe fica morarului o cheama Ana.'
+g1, g2, g3 = [], [], []
 
 with open(path, "r") as file:
-    for i in range(len(pattern)):
-        re.search(pattern[i], file)
+    for line in file:
+        g1 += p1.findall(line.strip())
+        g2 += p2.findall(line.strip())
+        g3 += p3.findall(line.strip())
 
-print(intregi, "\n", reale, "\n", cuv_majusc)
 
-# nu merge...(da eroare) dar regexurile sunt bune
+print('nr. fractionare:', g1, '\n', 'nr. intregi:', g2, '\n', 'cuv. cu majuscula:', g3)
