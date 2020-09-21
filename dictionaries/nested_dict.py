d6f23c305d147581a23f290eaa73e50653c2fd86
deeana325 <deeana.kris@gmail.com>
2020-09-21 09:54:39 +0300

    nested dict

---
+++ dictionaries/nested_dict.py
@@ -0,0 +1,6 @@
+romania = {'flora': {'lycopsida': 'lycopodiaceae', 'sphenopsida': 'equisetaceae', 'filicopsida': 'ophioglossaceae'},
+           'fauna': {'cyclostomata': 'lampreys', 'fish': 'sturgeon', 'amphibians': 'fire salamander'}}
+           
+print(romania)
+print(romania['fauna'])
+print(romania['fauna']['amphibians'])
